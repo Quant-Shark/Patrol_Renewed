8d7ba8a019610c2530ecd420225a7e49d6fa2fde
Nicolas Vlachos <blaxomania@gmail.com>
2016-03-30 00:50:28 +0300

    1st Alpha-Alpha-Alpha Commit

---
+++ WebApplication10/WebApplication10/Main.js
@@ -0,0 +1,19 @@
+﻿var canvas = document.getElementById('canvas');
+var Context = canvas.getContext('2d');
+
+var gameWidth = canvas.width;
+var gameHeight = canvas.height;
+
+
+
+var background = new Image();
+background.onload = function () {
+    Context.drawImage(background, 0, 0);
+};
+background.src = "background.jpg";
+
+var car = new Image();
+car.onload = function () {
+    Context.drawImage(car, 4, 400);
+};
+car.src = "car2.png";
